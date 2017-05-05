c5d75e6bdff2ce3e4716c32e95a6ffea60e8acd5
daidai0401 <daidai0401@gmail.com>
2017-05-04 23:51:34 -0400

    finish edit book feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < ApplicationController
+  
+  before_action :find_book, except: [:index, :new, :create]
+  
   def index
       @books = Book.all
     end
+  
   def new
       @book = Book.new
     end
+  
    def create
       @book = Book.new(book_params)
 
@@ -13,6 +18,21 @@ class BooksController < ApplicationController
       else
         render :new
       end
+    end
+  
+   def edit
+    end
+  
+   def update
+    if @book.update(book_params)
+    redirect_to books_path, notice: "updated!!"
+    else
+      render 'edit'
+    end
+     
+    def destroy
+      @book.destroy
+      redirect_to books_path, notice: "deleted!"
     end
   
   private
